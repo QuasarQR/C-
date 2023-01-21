6c894cb84f70e597a8604a50522cfcd5e7bafebd
Artem <artem.polehow@yandex.ru>
2023-01-21 21:48:06 +0300

    task 1

--- Lesson_1/First_sqr_of_second/Program.cs
+++ Lesson_1/First_sqr_of_second/Program.cs
@@ -1,3 +1,13 @@
-﻿Console.Write("Введите ваше имя: ");
-string username = Console.ReadLine();
-Console.WriteLine("Привет, " + username);
+﻿Console.Write("Введите первое число: ");
+int NumA = int.Parse (Console.ReadLine());
+Console.Write("Введите второе число: ");
+int NumB = int.Parse (Console.ReadLine());
+
+if (Math.Sqrt(NumA) == NumB)
+{
+Console.Write("Первое число является квадратом второго числа");
+}
+else
+{
+   Console.Write("Первое число не является квадратом второго числа");
+}
