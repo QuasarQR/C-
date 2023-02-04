a49060369e5a14ef01e7c2541ec6aa4a05b99957
Artem <artem.polehow@yandex.ru>
2023-02-04 19:15:32 +0300

    Lesson 3 complete issues

---
+++ Lesson_3/Summ_of_odds_position/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Задайте размер массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+int[] array = new int[arraySize];
+int sum = 0;
+
+for (int i = 0; i < arraySize; i++){
+   array[i] = new Random().Next(1, 101);
+}
+
+Console.WriteLine("Был задан массив из " + arraySize + " элементов, заполненный случайными числами от 1 до 100");
+
+for (int i = 0; i < arraySize; i++){
+   if (i % 2 > 0) sum += array[i];
+}
+
+Console.WriteLine("Сумма всех элементов массива на нечётной позиции равна: " + sum);
