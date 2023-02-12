1a47191c6a57a1e71b0c90d047c4af5a8364aa03
Artem <artem.polehow@yandex.ru>
2023-02-12 17:51:33 +0300

    lesson 4 complete

---
+++ Lesson_4/20_in_array/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите размер списка: ");
+int listSize = int.Parse(Console.ReadLine());
+List<int> numbers = new List<int> {};
+bool found = false;
+
+for (int i = 0; i < listSize; i++){
+   numbers.Add(new Random().Next(1, 21));
+   Console.Write(numbers[i] + " ");
+}
+Console.WriteLine("");
+
+for (int i = 0; i < numbers.Count; i++)
+{
+   if (numbers[i] == 20 && !found)
+   {
+      numbers[i] = 200;
+      found = true;
+   }
+}
+
+Console.WriteLine("Результат: ");
+foreach (int number in numbers)
+{
+   Console.Write(number + " ");
+}
---
+++ Lesson_4/Even_numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите размер массива: ");
+int arraySize = int.Parse(Console.ReadLine());
+int[] array = new int[arraySize];
+int even = 0;
+
+Console.WriteLine("Был задан массив размером " + arraySize + " элементов, заполненный случайными числами от 100 до 999");
+
+for (int i = 0; i < array.Length; i++){
+   array[i] = new Random().Next(100, 1000);
+   Console.Write(array[i] + " ");
+}
+Console.WriteLine("");
+
+for (int i = 0; i < array.Length; i++){
+   if (array[i] % 2 == 0) even ++;
+}
+
+Console.WriteLine("Количество чётных чисел в заданном массиве равно: " + even);
