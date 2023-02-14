39597ba74720f9a34c978ea1b92259af0b1614ef
Artem <artem.polehow@yandex.ru>
2023-02-14 17:15:20 +0300

    lesson 5 complete

---
+++ Lesson_5/Copy_array/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] sourceArray = { 1, 2, 3, 4, 5 };
+int[] destinationArray = new int[sourceArray.Length];
+
+Console.WriteLine("Был задан массив: {1, 2, 3, 4, 5}");
+for (int i = 0; i < sourceArray.Length; i++)
+{
+    destinationArray[i] = sourceArray[i];
+}
+
+Console.WriteLine("Скопированный массив: ");
+foreach (int number in destinationArray)
+{
+    Console.Write(number + " ");
+}
---
+++ Lesson_5/Scale_figure/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите координаты в формате (x,x) (x,x): ");
+string start = Console.ReadLine();
+List<string> list = new List<string>();
+List<string> result = new List<string>();
+
+Console.Write("Введите коэффициент масштабирования (0.5, 2, либо 4): ");
+string X = Console.ReadLine();
+double K = 0;
+if (X == "2") K = 2;
+if (X == "4") K = 4;
+if (X == "0.5") K = 0.5;
+
+
+foreach (string el in start.Split(" ")){
+   list.Add(el);
+}
+
+for (int i = 0; i < list.Count; i++){
+   string part1 = list[i][1].ToString();
+   string part2 = list[i][3].ToString();
+   double x = Convert.ToDouble(part1) * K;
+   double y = Convert.ToDouble(part2) * K;
+   result.Add("(" + x + ";" + y + ")");
+}
+
+foreach (string str in result)
+{
+    Console.Write(str + " ");
+}
